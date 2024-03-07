57fe0d3e3822a732b4b937f6085811741f93228b
Ural Ataullin <ataullinural@gmail.com>
2024-03-07 11:22:25 +0500

    registration is done

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
+from users import views
+
 app_name = 'users'
 
 urlpatterns = [
-    path('logout/', LogoutView.as_view(template_name='users/logged_out.html'),
+    path('logout/',
+         LogoutView.as_view(template_name='users/logged_out.html'),
          name='logout'),
+    path('signup/', views.SignUp.as_view(), name='signup')
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+# Функция reverse_lazy позволяет получить URL по параметрам функции path()
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from users.forms import CreationForm
+
+
+class SignUp(CreateView):
+    form_class = CreationForm
+    # После успешной регистрации перенаправляем пользователя на главную.
+    success_url = reverse_lazy('posts:index')
+    template_name = 'users/signup.html'
