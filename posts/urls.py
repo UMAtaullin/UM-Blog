7fe158fbefe1c917d85afa01498e87351d1f03c9
Ural Ataullin <ataullinural@gmail.com>
2024-03-12 08:46:46 +0500

    change groups

--- posts/urls.py
+++ posts/urls.py
@@ -6,7 +6,7 @@ app_name = 'posts'   # pylint: disable=invalid-name
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('group/<slug:any>/', views.group_posts, name='group_posts'),
+    path('group/<slug:slug>/', views.group_posts, name='group_posts'),
     path('profile/<str:username>/', views.profile, name='profile'),
     path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -29,7 +29,6 @@ def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:5]
     data = {
-        'title': 'Группы',
         'group': group,
         'posts': posts
     }
